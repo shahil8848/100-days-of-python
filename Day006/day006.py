2fe721585ef3eaf04ba248410c1a55f880267c81
shahil8848 <shahilbhusal2002@gmail.com>
2025-04-25 18:21:38 +0545

    Start of day6

---
+++ Day006/day006.py
@@ -0,0 +1,26 @@
+# functions , code blocks and while loops
+#what is functions
+#what is built in function
+
+print("hello") #this is also function
+print(len("hello"))
+
+def my_function():
+    print('hrllo')
+    print('byr')# it does not produce output
+
+def print_function():
+    print("Hello this is function with call ")
+
+print_function()#now it will print output why ?
+
+# how to create a function in python
+# def my_function():
+#Do this
+#Then do this
+#Finally do this
+
+#calling function
+#my_function()
+
+#indentation
---
+++ Day006/task.py
@@ -0,0 +1,25 @@
+# def reenrog_function():
+#     move()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#
+#
+# def reenrog2_function():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#
+#
+# for step in range(6):
+#     reenrog_function()
+#     reenrog2_function()
+#
+# #reeborg's world game
+
+#
