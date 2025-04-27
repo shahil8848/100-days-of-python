6098b065c71501e6fa1e6f2f26ea204cfd20e05c
shahil8848 <shahilbhusal2002@gmail.com>
2025-04-27 10:25:32 +0545

    Back from pause

--- Day006/day006.py
+++ Day006/day006.py
@@ -1,18 +1,18 @@
 # functions , code blocks and while loops
 #what is functions
 #what is built in function
+#
+# print("hello") #this is also function
+# print(len("hello"))
+#
+# def my_function():
+#     print('hrllo')
+#     print('byr')# it does not produce output
+#
+# def print_function():
+#     print("Hello this is function with call ")
 
-print("hello") #this is also function
-print(len("hello"))
-
-def my_function():
-    print('hrllo')
-    print('byr')# it does not produce output
-
-def print_function():
-    print("Hello this is function with call ")
-
-print_function()#now it will print output why ?
+# print_function()#now it will print output why ?
 
 # how to create a function in python
 # def my_function():
@@ -24,3 +24,16 @@ print_function()#now it will print output why ?
 #my_function()
 
 #indentation
+#what is indentation
+sky='blue'
+def my_weather():
+    if sky =='Clear':
+        print('blue')
+    elif sky =='Cloudy':
+        print('grey')
+    print('hello')
+print('world')
+# what is the output and why were others not printed
+
+
+
